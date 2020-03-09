bfbed3193a9960880423fc92f59dfcf0348d58a7
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-09 22:41:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,8 +28,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(string_array)
-  i = 0 
+
   if array.find do |type|
     string_array.include?(type) 
-  cheese_types = ["cheddar", "gouda", "camembert"]
+ 
 end
