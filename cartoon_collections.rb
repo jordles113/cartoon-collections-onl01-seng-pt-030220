f2865f8c17f54e0ad65f197969ce38208d94ee92
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-09 22:40:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,8 @@ def long_planeteer_calls(array)
   end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(string_array)
+  i = 0 
+  if array.include? {|i| i
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
