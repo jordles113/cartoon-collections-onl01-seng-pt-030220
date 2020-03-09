431c443da5edec4d70aca8efabe2117c2ebb3c7f
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-09 22:38:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,13 +17,14 @@ end
 new_array
 end
 
-def long_planeteer_calls(array)
+def long_planeteer_calls(array) 
   i = 0 
- if array.any? > {|i| i.length < 4}
-   true 
- else 
-   false 
- end 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i += 1  
+  end 
 end
 
 def find_the_cheese# code an argument here
